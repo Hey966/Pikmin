1086c05aa82e1845baff11510eec1f174c2626a9
Hey966 <jack01140206@gmail.com>
2026-05-23 15:58:40 +0800

    Include all countries in stats

--- js/stats.js
+++ js/stats.js
@@ -14,39 +14,39 @@ function renderStatsPage() {
     loadSavedRoutePlans();
   }
 
+  var countryMap = {};
   var regionMap = {};
   var districtMap = {};
-  var taiwanPoints = [];
   var favoriteCount = 0;
 
   for (var i = 0; i < points.length; i++) {
     var p = points[i];
+    var country = getPointCountry(p) || "其他";
+    var region = p.category || "未分類";
+    var district = country + "::" + region + "::" + (getPointDistrict(p) || "未分類區域");
 
-    if (getPointCountry(p) !== "台灣") {
-      continue;
+    countryMap[country] = true;
+    regionMap[country + "::" + region] = true;
+
+    if (country === "台灣") {
+      districtMap[district] = true;
     }
-
-    taiwanPoints.push(p);
 
     if (isPointFavorite(p)) {
       favoriteCount++;
     }
-
-    var region = p.category || "未分類";
-    var district = region + "::" + (getPointDistrict(p) || "未分類區域");
-
-    regionMap[region] = true;
-    districtMap[district] = true;
   }
 
+  var countries = countObjectKeys(countryMap);
   var regions = countObjectKeys(regionMap);
-  var districts = countObjectKeys(districtMap);
+  var taiwanDistricts = countObjectKeys(districtMap);
   var routePlanCount = typeof savedRoutePlans !== "undefined" ? savedRoutePlans.length : 0;
 
   var cards = [
-    { icon: "🌼", title: "總座標數", value: taiwanPoints.length },
-    { icon: "🏙️", title: "有資料縣市", value: regions },
-    { icon: "📍", title: "有資料區域", value: districts },
+    { icon: "🌼", title: "總座標數", value: points.length },
+    { icon: "🌍", title: "有資料國家", value: countries },
+    { icon: "🏙️", title: "有資料地區", value: regions },
+    { icon: "📍", title: "台灣有資料區域", value: taiwanDistricts },
     { icon: "⭐", title: "收藏座標", value: favoriteCount },
     { icon: "💾", title: "路線方案", value: routePlanCount }
   ];
@@ -61,9 +61,56 @@ function renderStatsPage() {
     html += '</article>';
   }
 
+  html += buildCountryStatsSection();
+
   summaryGrid.innerHTML = html;
 
-  renderLatestPoints(latestBox, taiwanPoints);
+  renderLatestPoints(latestBox, points);
+}
+
+function buildCountryStatsSection() {
+  var countryCounts = {};
+  var countryRegions = {};
+  var countryOrder = [];
+
+  for (var i = 0; i < points.length; i++) {
+    var p = points[i];
+    var country = getPointCountry(p) || "其他";
+    var region = p.category || "未分類";
+
+    if (!countryCounts[country]) {
+      countryCounts[country] = 0;
+      countryRegions[country] = {};
+      countryOrder.push(country);
+    }
+
+    countryCounts[country]++;
+    countryRegions[country][region] = true;
+  }
+
+  if (countryOrder.length === 0) {
+    return "";
+  }
+
+  countryOrder.sort();
+
+  var html = '<section class="stats-country-breakdown">';
+  html += '<h3>🌍 各國座標統計</h3>';
+  html += '<div class="stats-country-list">';
+
+  for (var j = 0; j < countryOrder.length; j++) {
+    var countryName = countryOrder[j];
+    html += '<article class="stats-country-item">';
+    html += '<b>' + escapeHTML(countryName) + '</b>';
+    html += '<span>' + countryCounts[countryName] + ' 筆座標</span>';
+    html += '<small>' + countObjectKeys(countryRegions[countryName]) + ' 個地區</small>';
+    html += '</article>';
+  }
+
+  html += '</div>';
+  html += '</section>';
+
+  return html;
 }
 
 function renderLatestPoints(container, sourcePoints) {
@@ -86,7 +133,7 @@ function renderLatestPoints(container, sourcePoints) {
 
     html += '<article class="stats-latest-item">';
     html += '<div class="stats-latest-title">🌼 ' + escapeHTML(p.name || "未命名巨大花朵") + '</div>';
-    html += '<div class="stats-latest-meta">' + escapeHTML(p.area || "未填寫區域") + '</div>';
+    html += '<div class="stats-latest-meta">' + escapeHTML(getPointCountry(p)) + '｜' + escapeHTML(p.area || "未填寫區域") + '</div>';
     html += '<div class="stats-latest-meta">' + escapeHTML(p.y) + ', ' + escapeHTML(p.x) + '</div>';
     html += '</article>';
   }
